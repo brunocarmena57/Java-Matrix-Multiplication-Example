154bea8da2e91d501c04c5463dde047efbd7c074
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-03-15 19:09:17 -0300

    Add files via upload

---
+++ java1503.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Exercicio12{
+	public static void main(String args[]){
+
+		//variáveis
+		int n, m, k;
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Insira tres valores: ");
+        n = leitor.nextInt();
+        m = leitor.nextInt();
+        k = leitor.nextInt();
+
+		int[][] mat = new int[n][m];
+		int[][] mat2 = new int[m][k];  
+		int[][] mat3 = new int[n][k];        
+        
+		System.out.println("Insira os valores da primeira matriz [" + n + " x " + m + "]");
+        for(int i=0; i < n; i++){
+            for(int j=0; j < m; j++){
+                mat[i][j] = leitor.nextInt();
+            }   
+        }
+        System.out.println("Insira os valores da segunda matriz [" + m + " x " + k + "]");
+        for(int i=0; i < m; i++){
+                for(int j=0; j < k; j++){
+                    mat2[i][j] = leitor.nextInt();
+            }                   
+        }     
+
+        for(int i=0; i < m; i++){
+            for(int j=0; j < m; j++){
+                mat3[i][j] = (mat[i][j] * mat2[j][i]);
+            }                   
+            System.out.println();
+        }  
+        System.out.println();
+        for(int i=0; i < m; i++){
+            for(int j=0; j < k; j++){
+                System.out.print(mat2[i][j] + " ");
+            }                   
+            System.out.println();
+        }  
+    }
+}
